9ea1f76bb708b8e0637067065364691eb8ca29c5
tylerliu <tylersiqi@163.com>
2020-10-17 11:53:36 -0700

    try boost 1.65
    Change-Id: I425bab159a778455fb4bcc6144274c2c068f42de

--- tools/ndncert-ca-server.cpp
+++ tools/ndncert-ca-server.cpp
@@ -39,7 +39,11 @@ std::string repoPort = "7376";
 static bool
 writeDataToRepo(const Data& data) {
   boost::asio::ip::tcp::iostream requestStream;
-  requestStream.expires_after(std::chrono::seconds(3));
+#if BOOST_VERSION >= 106600
+    requestStream.expires_after(std::chrono::seconds(3));
+#else
+    requestStream.expires_at(std::chrono::seconds(3));
+#endif
   requestStream.connect(repoHost, repoPort);
   if (!requestStream) {
     std::cerr << "ERROR: Cannot publish certificate to repo-ng"
